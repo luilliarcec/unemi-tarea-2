7e26d9f66d085e396ab97c34de0ae02e2be886cc
Luis Arce <luilliarcec@gmail.com>
2025-10-06 08:26:25 -0500

    Fix route typo: add missing slash in `/contacts/{contact}` delete endpoint.

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/contacts', [ContactController::class, 'index']);
-Route::delete('/contacts{contact}', [ContactController::class, 'destroy']);
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
