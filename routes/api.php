28c646cc63840f552d78f4fa7d8c67bf1443b5b0
Luis Arce <luilliarcec@gmail.com>
2025-10-06 14:18:08 -0500

    Add POST endpoint for creating new contacts

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/contacts', [ContactController::class, 'index']);
+Route::post('/contacts', [ContactController::class, 'store']);
 Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
